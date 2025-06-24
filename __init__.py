222dc569829276e55ed0d859249ed77ccd369b53
Mohamed Zeidan <mohamedzeidan2012@gmail.com>
2025-06-24 20:48:34 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,26 @@
+# Physics/__init__.py
+
+"""
+Physics Package
+A full open-source source code of how the real world and sound work, based on physics.
+"""
+
+from . import (
+    constants,
+    units,
+    mechanics,
+    sound,
+    waves,
+    thermodynamics,
+    electromagnetism,
+    relativity,
+    quantum,
+    nuclear,
+    optics,
+    fields,
+    astrophysics,
+    particles,
+    chaos,
+    math_tools,
+    philosophy
+)
